f554725db673b1d1bad79dcb90863ec1f5ccf1aa
Malcolm Laing <malcolm.laing@commercetools.de>
2019-01-02 14:53:39 +0100

    chore: show pptr

--- src/components/tag/tag.visualroute.js
+++ src/components/tag/tag.visualroute.js
@@ -11,57 +11,78 @@ quam pharetra, posuere mauris in, pharetra nisi.`;
 
 export const routePath = '/tag';
 
-export const component = () => (
-  <Suite>
-    <Spec label="Normal">
-      <Tag type="normal">Tag</Tag>
-    </Spec>
-    <Spec label="Normal - onRemove">
-      <Tag type="normal" onRemove={() => {}}>
-        With remove
-      </Tag>
-    </Spec>
-    <Spec label="Normal - horizontalConstraint - xs">
-      <Tag type="normal" horizontalConstraint="xs">
-        Tag
-      </Tag>
-    </Spec>
-    <Spec label="Normal - horizontalConstraint - s">
-      <Tag type="normal" horizontalConstraint="s">
-        Tag
-      </Tag>
-    </Spec>
-    <Spec label="Normal - horizontalConstraint - m">
-      <Tag type="normal" horizontalConstraint="m">
-        Tag
-      </Tag>
-    </Spec>
-    <Spec label="Normal - horizontalConstraint - l">
-      <Tag type="normal" horizontalConstraint="l">
-        Tag
-      </Tag>
-    </Spec>
-    <Spec label="Normal - horizontalConstraint - xl">
-      <Tag type="normal" horizontalConstraint="xl">
-        Tag
-      </Tag>
-    </Spec>
+class Component extends React.Component {
+  state = {
+    showRemovableTag: true,
+  };
 
-    <Spec label="Warning">
-      <Tag type="warning">Warning message</Tag>
-    </Spec>
-    <Spec label="Warning - disabled">
-      <Tag type="warning" isDisabled={true}>
-        Warning but disabled
-      </Tag>
-    </Spec>
-    <Spec label="Normal - multiple lines of text">
-      <Tag type="normal">{longText}</Tag>
-    </Spec>
-    <Spec label="Normal - multiple lines of text - onRemove">
-      <Tag type="normal" onRemove={() => {}}>
-        {longText}
-      </Tag>
-    </Spec>
-  </Suite>
-);
+  onToggle = () => {
+    this.setState(prevState => ({
+      showRemovableTag: !prevState.showRemovableTag,
+    }));
+  };
+  render() {
+    return (
+      <Suite>
+        <Spec label="Normal">
+          <Tag type="normal">Tag</Tag>
+        </Spec>
+        {this.state.showRemovableTag && (
+          <Spec label="Normal - onRemove">
+            <Tag
+              type="normal"
+              onRemove={this.onToggle}
+              data-testid="removable-tag"
+            >
+              With remove
+            </Tag>
+          </Spec>
+        )}
+        <Spec label="Normal - horizontalConstraint - xs">
+          <Tag type="normal" horizontalConstraint="xs">
+            Tag
+          </Tag>
+        </Spec>
+        <Spec label="Normal - horizontalConstraint - s">
+          <Tag type="normal" horizontalConstraint="s">
+            Tag
+          </Tag>
+        </Spec>
+        <Spec label="Normal - horizontalConstraint - m">
+          <Tag type="normal" horizontalConstraint="m">
+            Tag
+          </Tag>
+        </Spec>
+        <Spec label="Normal - horizontalConstraint - l">
+          <Tag type="normal" horizontalConstraint="l">
+            Tag
+          </Tag>
+        </Spec>
+        <Spec label="Normal - horizontalConstraint - xl">
+          <Tag type="normal" horizontalConstraint="xl">
+            Tag
+          </Tag>
+        </Spec>
+
+        <Spec label="Warning">
+          <Tag type="warning">Warning message</Tag>
+        </Spec>
+        <Spec label="Warning - disabled">
+          <Tag type="warning" isDisabled={true}>
+            Warning but disabled
+          </Tag>
+        </Spec>
+        <Spec label="Normal - multiple lines of text">
+          <Tag type="normal">{longText}</Tag>
+        </Spec>
+        <Spec label="Normal - multiple lines of text - onRemove">
+          <Tag type="normal" onRemove={() => {}}>
+            {longText}
+          </Tag>
+        </Spec>
+      </Suite>
+    );
+  }
+}
+
+export const component = Component;
--- src/components/tag/tag.visualspec.js
+++ src/components/tag/tag.visualspec.js
@@ -1,12 +1,25 @@
 import { percySnapshot } from '@percy/puppeteer';
+import { getDocument, queries } from 'pptr-testing-library';
+
+const { getByText, getByLabelText, queryByTestId } = queries;
 
 describe('Tag', () => {
   beforeAll(async () => {
     await page.goto(`${HOST}/tag`);
   });
 
+  it('Can remove a tag', async () => {
+    const doc = await getDocument(page);
+    const removeableTag = await getByLabelText(doc, 'Remove');
+    await removeableTag.click();
+    const removedTag = await queryByTestId(doc, 'tag-removable');
+    expect(removedTag).not.toBeTruthy();
+  });
+
   it('Default', async () => {
-    await expect(page).toMatch('Normal');
+    const doc = await getDocument(page);
+    const tag = await getByText(doc, 'Tag');
+    expect(tag).toBeTruthy();
     await percySnapshot(page, 'Tag');
   });
 });
